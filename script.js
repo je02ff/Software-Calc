79fc9ee25884da0f1ae2d1cc9628561490639ebe
Parker Hague <parkerhague@gmail.com>
2021-11-10 21:39:52 -0600

    added clear_all() and digit_pressed() functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//
+
+digits_pressed = 0;
+
+
+function calculate_entries(string_to_caluclate) {
+
+    // need to implement eval() func here
+
+}
+
+function digit_pressed() {
+    digits_pressed++;
+    console.log(digits_pressed)
+
+    if (digits_pressed > 8) {
+        calc_form.display.value = 'ERR'
+        digits_pressed = 0;
+
+    }
+}
+
+function clear_all() {
+    calc_form.display.value = ' '
+    digits_pressed = 0;
+}
